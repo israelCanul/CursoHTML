793d8224492b0318e3dd7cd056885413298e0c32
Israel Canul <israel.canul@gmail.com>
2016-09-09 18:01:41 -0500

    ultimo cambios

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,34 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+var scss = require('postcss-scss');
+var nested = require('postcss-nested');
+var postcss = require('gulp-postcss');
 
 // for master pages
 var hbsmaster = require('gulp-handlebars-master');
 var rename = require('gulp-rename');
 
+gulp.task('css1', function() {
+  gulp.src('scss/app.scss')
+  .pipe(autoprefixer()
+  )
+  .pipe(sass({
+    includePaths: ['scss']
+  }))
+  .pipe(gulp.dest('app/css'));
+});
+
 gulp.task('css', function() {
-  gulp.src('css/app.css')
+  gulp.src('scss/app.scss')
     .pipe(autoprefixer()
     )
-    .pipe(gulp.dest('app/css'));
+    .pipe(sass({
+      includePaths: ['scss']
+    }))
+    .pipe(gulp.dest('app/css/'));
 });
+
 
 // watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
 gulp.task('serve', ['css'], function() {
@@ -24,18 +41,16 @@ gulp.task('serve', ['css'], function() {
       baseDir: 'app'
     }
   });
-
 });
 
 gulp.task('watch', ['css','handlebars', 'serve'], function() {
-  gulp.watch(["css/*.css"], ['css']);
+  gulp.watch(["scss/*.scss"], ['css']);
   gulp.watch(["pages/*.hbs"], ['handlebars']);
   gulp.watch(["pages/sections/*.hbs"], ['handlebars']);
 });
 
 
 gulp.task('handlebars', function() {
-
     var templatedata = {
 	  "index" : {
 	    "title" : "Home Page"
@@ -47,7 +62,6 @@ gulp.task('handlebars', function() {
 	    "title" : "Colores"
 	  },
     };
-
     gulp.src('./pages/sections/*.hbs')
 	  .pipe( hbsmaster('./pages/master.hbs', templatedata, {}))
 	  .pipe( rename( function(path){
